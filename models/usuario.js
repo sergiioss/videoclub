4e278875cf1c276255c15d54f8b42cbc35330b8b
sergiioss <caos13es@hotmail.com>
2022-06-10 10:06:30 +0200

    Password encriptado y probado

--- models/usuario.js
+++ models/usuario.js
@@ -20,6 +20,7 @@ module.exports = (sequelize, DataTypes) => {
   Usuario.init({
     nombre: DataTypes.STRING,
     dni: DataTypes.STRING,
+    password: DataTypes.STRING,
     email: DataTypes.STRING,
     telefono: DataTypes.STRING
   }, {
